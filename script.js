bfe96a10e79b1aee863f223b4ad5325adea80fe4
Massoud Rahmani <mrahmani@LABA.PSWAS.LOCAL>
2023-08-02 16:21:33 -0700

    Optional challenge

---
+++ script.js
@@ -0,0 +1,65 @@
+
+
+// 1. let numberOfStudents = 17;
+// Check if we have more than 20 students, if yes console log "classroom too small"else console log "classroom size acceptable
+
+// let numberOfStudents = 17;
+// if(numberOfStudents > 20){
+//     console.log("classroom too small")
+// } else {
+//     console.log("classroom size acceptable")
+// };
+
+
+// 2. 
+
+// let numberOfHouses = 5;
+
+// Check if this person’s number of houses is greater than 2 - if true console.log(“you need to sell more houses”)
+
+// If 2 or less houses - log “seems reasonable
+
+// let numberOfHouses = 1;
+
+// if(numberOfHouses > 2){
+//     console.log("you need to sell more houses")
+// } else if (numberOfHouses <= 2) {
+//     console.log("seems reasonable")
+// }
+
+
+// 3. 
+
+// let bill = 20;
+
+// Check if the bill is higher than 15 if yes “we should split the bill” 
+
+// If not higher than 15 “I’ll get the bill this time”
+
+
+// let bill = 10;
+
+// if(bill > 15 && bill !== 15){
+//     console.log("we should split the bill")
+// } else
+//     console.log("I'll get the bill this time")
+// change this aapproch, and modify the code. 
+
+let studentScore = 900;
+
+if(studentScore <= 55 ){
+    console.log("not to good")
+} else if(studentScore <= 60 ){
+    console.log("ehhhh")
+} else if(studentScore <= 70){
+    console.log("Keep trying")
+} else if(studentScore <= 80){
+    console.log("nice")
+} else if(studentScore <= 90){
+    console.log("Doing good")
+} else {
+    console.log("great")
+}
+
+
+
